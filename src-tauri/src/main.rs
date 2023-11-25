35a268f7c47ed78d29f64b921d25f8541d628709
soruse <piotrmolkontakt@gmail.com>
2023-11-25 11:44:56 +0100

    ✨ initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,55 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{CustomMenuItem, Manager, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{SystemTray, SystemTrayEvent};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn handle_keydown(key: String) {
+    // Handle the keydown event here
+    println!("Key pressed: {}", key);
+}
+
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
+    let tray = SystemTray::new().with_menu(tray_menu);
+
+    tauri::Builder::default()
+        .on_window_event(|event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event.event() {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+        })
+        .invoke_handler(tauri::generate_handler![greet, handle_keydown])
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+                let item_handle = app.tray_handle().get_item(&id);
+                match id.as_str() {
+                    "hide" => {
+                        let window = app.get_window("main").unwrap();
+                        window.hide().unwrap();
+                        // you can also `set_selected`, `set_enabled` and `set_native_image` (macOS only).
+                        item_handle.set_title("Show").unwrap();
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("error while building tauri application");
+}
